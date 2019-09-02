f6e41ad3ec57caf99af9a28fee62be938630e4d7
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-09-02 10:18:01 -0700

    added multi messaging

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 
 const WebSocket = require('ws');
 
+const connections = [];
+
 const ws = new WebSocket.Server( {
     'port': 8888
 });
 
 ws.on('connection', function( conn ){
-    console.log('got a connection!');
+    connections.push( conn );
+    console.log('got a connection!, count: ' + connections.length);
     conn.on('message', function( message ){
         console.log('got a message!', message);
-        conn.send('echo: ' + message);
+        for( var connIndex = connections.length-1; connIndex>=0; connIndex--){
+            connections[connIndex].send( message );
+        }
     })
     conn.on('close', function(){
         console.log('connection closed!')
